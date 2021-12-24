1f1407829f6b08135fbb23e30a9714f47fdc867e
Kamran Ayub <kamran.ayub@gmail.com>
2021-12-24 00:24:22 -0600

    Test and create nuget package

--- plugins/prepare-nuget-release.js
+++ plugins/prepare-nuget-release.js
@@ -1,3 +1,9 @@
+// DO NOT USE
+//
+// We ultimately might need to fork and fix semantic-release-nuget
+// package to properly pass the /p:Version value by default
+// from the Commit Analyzer Semantic Release plugin.
+//
 module.exports = {
   verifyConditions: (pluginConfig, { logger, nextRelease: { version } }) => {
     logger.info("Setting NUGET_VERSION env variable", version);
