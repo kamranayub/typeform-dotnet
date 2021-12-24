89b70e817fe95ae2bc58fa8f1283e61e35b5e6ea
Kamran Ayub <kamran.ayub@gmail.com>
2021-12-23 23:52:44 -0600

    add release workflow; wip semantic-release

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-TAG=$1
-KEY=$2
-
-dotnet pack -c Release -o ../dist Typeform/Typeform.csproj -p:PackageVersion=$TAG -p:NuspecFile=../Typeform.nuspec
-
-dotnet nuget push dist/Typeform.$TAG.nupkg -k $KEY -s https://www.nuget.org
