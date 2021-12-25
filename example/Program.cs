3cbd6329eb15bd3720fa3259fdcd610c7339479b
Kamran Ayub <kamran.ayub@gmail.com>
2021-12-24 20:25:33 -0600

    Add simple consumption example

---
+++ example/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Typeform;
+
+var accessToken = System.Environment.GetEnvironmentVariable("TYPEFORM_ACCESS_TOKEN");
+
+var typeformApi = TypeformClient.CreateApi();
+var responses = await typeformApi.GetFormResponsesAsync(accessToken, "xVMHX23n");
+
+Console.WriteLine($"Found {responses.TotalItems}");
